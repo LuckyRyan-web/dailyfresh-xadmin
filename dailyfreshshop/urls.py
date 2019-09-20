307bbb03cef7a23be06cc960627e52abed51b5fa
373005226 <373005226@qq.com>
2019-09-20 20:41:24 +0800

    更新了购物车功能

--- dailyfreshshop/urls.py
+++ dailyfreshshop/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     url('search/', include('haystack.urls')),  # 全文检索框架
     url(r'^captcha/', include('captcha.urls')),         # 验证码
     url(r'', include('goods.urls', namespace='goods')),  # 商品模块
+    url('cart/', include('cart.urls', namespace='cart')),  # 购物车模块
+    # url('order/', include('order.urls', namespace='order')),  # 订单模块
     # url(r'^index/$|index.html/$|^$', IndexView.as_view(), name='index'),
     url(r'^active/(?P<token>.*)$', AciveUserView.as_view(), name='active'),  # 用户激活
     url(r'^user/',include('user.urls',namespace='user')),
